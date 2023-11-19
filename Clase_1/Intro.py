5a3133084d2bae7458828b09e32c7d7980a75a5f
CfBs919 <carlossaavedra255@gmail.com>
2023-11-19 13:10:18 -0500

    subiendo archivos de clase introductoria

---
+++ Clase_1/Intro.py
@@ -0,0 +1,9 @@
+print("Hola mundo") 
+fecha=input("ingrese su fecha de nacimiento : ") 
+print (fecha) 
+print("la fecha ingresada es : "+fecha) 
+print("ingrese 2 numeros:")
+num1 = 5 
+num2 = 7.2 
+nombre = "Richard" 
+print (num1 + num2)
